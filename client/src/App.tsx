45ed9e1b314b2b40628268ea7baf80979f14edda
NureTsymbalMilena <cymbalale13@gmail.com>
2026-06-14 20:24:02 +0300

    feat (client): add configs

---
+++ client/src/App.tsx
@@ -0,0 +1,85 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout/MainLayout.tsx";
+import {CoursesPage, EditCoursePage} from "./features/courses";
+import {ProfilePage} from "./features/profile";
+import {
+    EmailVerificationPage as AuthEmailVerificationPage,
+    ForgotPasswordPage,
+    LoginPage,
+    ResetPasswordPage
+} from "./features/auth";
+import {
+    ParentEmailVerificationPage,
+    HeadmasterEmailVerificationPage,
+    ParentCodePage,
+    ParentDetailsPage, SchoolDetailsPage, SchoolSelectionPage, SchoolDocumentsPage,
+} from "./features/registration";
+import React from "react";
+import {ToastContainer} from "react-toastify";
+import {CreateCoursePage} from "@/features/courses";
+import {CreateAnnouncementPage, EditAnnouncementPage} from "@/features/announcements";
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={4000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+
+                <Route path="/password/forgot-password" element={<ForgotPasswordPage />} />
+                <Route path="/password/verify-email" element={<AuthEmailVerificationPage />} />
+                <Route path="/password/reset-password" element={<ResetPasswordPage />} />
+
+                <Route path="/parent/register/init" element={<ParentCodePage />} />
+                <Route path="/parent/register/details" element={<ParentDetailsPage />} />
+                <Route path="/parent/verify-email" element={<ParentEmailVerificationPage />} />
+
+                <Route path="/school/register/init" element={<SchoolSelectionPage />} />
+                <Route path="/school/register/details" element={<SchoolDetailsPage />} />
+                <Route path="/school/verify-email" element={<HeadmasterEmailVerificationPage />} />
+                <Route path="/school/register/submit" element={<SchoolDocumentsPage />} />
+
+                <Route path="/" element={<MainLayout />}>
+                    <Route path="/courses" element={<CoursesPage />} />
+                    <Route path="/courses/create" element={<CreateCoursePage />} />
+                    <Route path="/courses/:courseId/edit" element={<EditCoursePage />} />
+
+                    <Route path="/announcements/create" element={<CreateAnnouncementPage />} />
+                    <Route path="/announcements/:announcementId/edit" element={<EditAnnouncementPage />} />
+
+
+                    {/*<Route path="/chats" element={<Chats />} />*/}
+                    <Route path="/profile" element={<ProfilePage />} />
+                    {/*<Route path="/statistics" element={<Statistics />} />*/}
+                    {/*<Route path="/admin" element={<Admin />} />*/}
+                    {/*<Route path="/course" element={<Course />} />*/}
+                    {/*<Route path="/create-course" element={<CreateCourse />} />*/}
+                    {/*<Route path="/edit-course" element={<EditCoursePage />} />*/}
+                    {/*<Route path="/task" element={<Task />} />*/}
+                    {/*<Route path="/create-task" element={<CreateTask />} />*/}
+                    {/*<Route path="/edit-task" element={<EditTask />} />*/}
+                    {/*<Route path="/test" element={<Test />} />*/}
+                    {/*<Route path="/create-test" element={<CreateTest />} />*/}
+                    {/*<Route path="/edit-test" element={<EditTest />} />*/}
+                    {/*<Route path="/take-test" element={<TakeTest />} />*/}
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App
---
+++ client/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+      react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        replaceAttrValues: {
+          '#000': 'currentColor',
+          '#000000': 'currentColor',
+          '#202020': 'currentColor',
+          '#F13E3E': 'currentColor',
+          '#f13e3e': 'currentColor',
+          'red': 'currentColor'
+        }
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
