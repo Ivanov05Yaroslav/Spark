b3047a8b6106b5b8cc42a1eebefe236ba2458a96
NureTsymbalMilena <cymbalale13@gmail.com>
2026-06-15 22:48:09 +0300

    feat (client): add auth inteceptor

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -6,3 +6,68 @@ export const apiClient = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+let refreshTokenPromise: Promise<string | null> | null = null;
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._isRetry) {
+            originalRequest._isRetry = true;
+
+            if (!refreshTokenPromise) {
+                refreshTokenPromise = (async () => {
+                    const refreshToken = localStorage.getItem('refreshToken');
+
+                    if (!refreshToken) return null;
+
+                    try {
+                        const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/refresh`, {
+                            refreshToken: refreshToken
+                        });
+
+                        localStorage.setItem('accessToken', data.accessToken);
+                        localStorage.setItem('refreshToken', data.refreshToken);
+
+                        return data.accessToken;
+                    } catch (refreshError) {
+                        console.error('Сесія застаріла, вихід з аккаунту...');
+                        localStorage.removeItem('accessToken');
+                        localStorage.removeItem('refreshToken');
+
+                        window.location.href = '/login';
+                        return null;
+                    } finally {
+                        refreshTokenPromise = null;
+                    }
+                })();
+            }
+
+            const newAccessToken = await refreshTokenPromise;
+
+            if (newAccessToken) {
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                return apiClient(originalRequest);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
